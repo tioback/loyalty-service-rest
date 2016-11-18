71b18d385520f8265f238e11d3e8fcfc2fba6a7b
Renato Pereira Back <renato.back@micromed.com.br>
2016-11-18 17:39:17 -0200

    Tentativa de sincronizar hora

--- src/main/java/br/ufsc/grad/renatoback/tcc/loyalty/service/rest/LoyaltyService.java
+++ src/main/java/br/ufsc/grad/renatoback/tcc/loyalty/service/rest/LoyaltyService.java
@@ -11,24 +11,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoyaltyService {
 
+	private static final Long BASE_TIME = 379089900000l;
+
 	private Log logger = LogFactory.getLog(LoyaltyService.class);
 
-	AtomicInteger counter = new AtomicInteger();
-	AtomicLong average = new AtomicLong();
+	private AtomicInteger counter = new AtomicInteger();
+	private AtomicLong average = new AtomicLong();
+	private Long serverDiff;
 
 	public void createBalance(Long time) {
 		counter.incrementAndGet();
-		average.accumulateAndGet(new Date().getTime() - time, (n, m) -> (n + m) / (n == 0 || m == 0 ? 1 : 2));
+		average.accumulateAndGet(new Date().getTime() - time + serverDiff,
+				(n, m) -> (n + m) / (n == 0 || m == 0 ? 1 : 2));
 	}
 
 	public void printStatistics(Integer threads, Integer sleep) {
-		logger.info(String.format("LOYALTY [%d threads][%d sleep] - [%d packages][%d average ms]", threads, sleep,
+		logger.info(String.format("LOYALTY [%d threads][%d sleep] - [%d subscriptions][%d average ms]", threads, sleep,
 				counter.get(), average.get()));
 	}
 
 	public void resetStatistics() {
 		counter.set(0);
 		average.set(0);
+	}
+
+	public void syncTime(Long remoteDiff) {
+		long localDiff = System.currentTimeMillis() - BASE_TIME;
+		serverDiff = remoteDiff - localDiff;
 	}
 
 }
